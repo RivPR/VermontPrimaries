e4a28fac7d1bcbbb7944884e864afd8c95f97b61
Angel Moreira <amoreira@thunderhead.com>
2018-07-08 01:10:20 -0400

    Added mongoose for db

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ var app = express();
 
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+var mongoConnectString = `mongodb://${(process.env.MONGO_HOST || "localhost:27017")}/vt_prim`;
+// create the DB connection
+mongoose.connect(mongoConnectString);
+var db = mongoose.connection;
+
+db.once('open', function(){
+    console.log("Mongoose connected to " + mongoConnectString + ".");
+});
+db.on('error', console.error.bind(console, 'connection error: '));
+
 var server = app.listen(process.env.PORT || 8080, function(){
     console.log("App up on... %s...", server.address().port);
 });
